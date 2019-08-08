78ae8020a6fcb8f7ae7d262df360cd804d52b725
Arlene George <aemg@live.com>
2019-08-08 03:51:54 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,10 @@
-first_number = 0
+first_number = 2
 second_number = 6 
 
 sum = first_number + second_number
+
+difference = second_number - first_number
+
+product = first_number * second_number
+
+quotient = second_number / first_number
